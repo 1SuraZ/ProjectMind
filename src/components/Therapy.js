f4774280afb638463eafac403e02b16994215dd8
1SuraZ <131213619+1SuraZ@users.noreply.github.com>
2024-05-18 23:36:50 -0400

    These are the front end client files with react and js
    front end push filles for testing to developers

---
+++ src/components/Therapy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Therapy() {
+  return (
+    <div>
+      <h1>Therapy Services</h1>
+      <p>Information about therapy services.</p>
+    </div>
+  );
+}
+
+export default Therapy;
---
+++ src/pages/PeerSupport.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PeerSupport() {
+  return (
+    <div>
+      <h1>Peer Support</h1>
+      <p>Information about peer support.</p>
+    </div>
+  );
+}
+
+export default PeerSupport;
