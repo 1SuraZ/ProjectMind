8a3dd97a78e8a1df6a6f34672c157429cdbfadb6
1SuraZ <131213619+1SuraZ@users.noreply.github.com>
2024-05-20 16:11:08 -0400

    client named
    make a new folder named clients and put all the outside files of code except the server folder

---
+++ src/pages/PeerSupport.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PeerSupport = () => {
+  return (
+    <div>
+      <h1>Peer Support</h1>
+      <p>Information about peer support.</p>
+    </div>
+  );
+}
+
+export default PeerSupport;
