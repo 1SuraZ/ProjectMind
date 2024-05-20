18cb2d31aaaddae967b3b1fc3140d3bb4242862f
1SuraZ <131213619+1SuraZ@users.noreply.github.com>
2024-05-20 15:54:50 -0400

    push
    push

---
+++ src/pages/BeMindful.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BeMindful() {
+  return (
+    <div>
+      <h1>Be Mindful</h1>
+      <p>Information about mindfulness and spirituality.</p>
+    </div>
+  );
+}
+
+export default BeMindful;
